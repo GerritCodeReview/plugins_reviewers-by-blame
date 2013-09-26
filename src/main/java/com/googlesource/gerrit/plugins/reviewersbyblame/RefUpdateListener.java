b9a74badc845832257ee3b785f776876805bf65b
David Ostrovsky <david@ostrovsky.org>
2013-09-26 21:12:58 +0200

    Support configuration of plugin in one site wide config file
    In If5b1a56ae3df964154bc6c6b2487ada7fea49650 configuration of this plugin was
    enabled on project level in `project.config`. This change extends the concept
    and supports optionally also one config file per site. If this file doesn't
    exist then the configuration is falled back to `project.config` file on project
    level.
    
    Change-Id: Ic19bb94992890f6af8bc654504f28a527753d366

--- src/main/java/com/googlesource/gerrit/plugins/reviewersbyblame/RefUpdateListener.java
+++ src/main/java/com/googlesource/gerrit/plugins/reviewersbyblame/RefUpdateListener.java
@@ -14,15 +14,20 @@
 
 package com.googlesource.gerrit.plugins.reviewersbyblame;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.util.FS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,10 +35,12 @@ import com.google.gerrit.extensions.events.GitReferenceUpdatedListener;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.reviewdb.client.Project.NameKey;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.gerrit.server.config.SitePaths;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.WorkQueue;
 import com.google.gerrit.server.project.NoSuchProjectException;
@@ -49,6 +56,8 @@ class RefUpdateListener implements GitReferenceUpdatedListener {
 
   private static final Logger log = LoggerFactory
       .getLogger(RefUpdateListener.class);
+  private final static String SECTION_PROJECT = "project";
+  private final static String MAX_REVIEWERS = "maxReviewers";
 
   private final ReviewersByBlame.Factory reviewersByBlameFactory;
   private final GitRepositoryManager repoManager;
@@ -56,7 +65,8 @@ class RefUpdateListener implements GitReferenceUpdatedListener {
   private final IdentifiedUser.GenericFactory identifiedUserFactory;
   private final ThreadLocalRequestContext tl;
   private final SchemaFactory<ReviewDb> schemaFactory;
-  private final PluginConfigFactory cfg;
+  private final PluginConfigFactory pluginCfg;
+  private final ProjectConfiguration config;
   private ReviewDb db;
 
   @Inject
@@ -65,36 +75,115 @@ class RefUpdateListener implements GitReferenceUpdatedListener {
       final IdentifiedUser.GenericFactory identifiedUserFactory,
       final ThreadLocalRequestContext tl,
       final SchemaFactory<ReviewDb> schemaFactory,
-      final PluginConfigFactory cfg) {
+      final PluginConfigFactory pluginCfg,
+      final SitePaths site) {
     this.reviewersByBlameFactory = reviewersByBlameFactory;
     this.repoManager = repoManager;
     this.workQueue = workQueue;
     this.identifiedUserFactory = identifiedUserFactory;
     this.tl = tl;
     this.schemaFactory = schemaFactory;
-    this.cfg = cfg;
+    this.pluginCfg = pluginCfg;
+    this.config = get(readConfigFileFor(site));
+  }
+
+  private interface ProjectConfiguration {
+    boolean isEnabledFor(Project.NameKey p);
+    int getMaxReviewersFor(Project.NameKey p);
+  }
+
+  private ProjectConfiguration get(final Config siteConfig) {
+    if (siteConfig != null) {
+      final boolean explicitlyEnableProjects = siteConfig
+          .getBoolean("gerrit", null, "explicitlyEnableProjects", true);
+      return new ProjectConfiguration() {
+        @Override
+        public boolean isEnabledFor(NameKey p) {
+          if (!explicitlyEnableProjects) {
+            return true;
+          }
+          return siteConfig.getBoolean(SECTION_PROJECT, p.get(),
+              "enabled", false);
+        }
+
+        @Override
+        public int getMaxReviewersFor(NameKey p) {
+          return siteConfig.getInt(SECTION_PROJECT, p.get(),
+              MAX_REVIEWERS, 3);
+        }
+      };
+    } else {
+      return new ProjectConfiguration() {
+        @Override
+        public boolean isEnabledFor(NameKey p) {
+          int maxReviewers;
+          try {
+            maxReviewers = readEntry(p);
+          } catch (NoSuchProjectException x) {
+            log.error(x.getMessage(), x);
+            return false;
+          }
+          return maxReviewers > 0;
+        }
+
+        @Override
+        public int getMaxReviewersFor(NameKey p) {
+          try {
+            return readEntry(p);
+          } catch (NoSuchProjectException x) {
+            // already checked
+            return 0;
+          }
+        }
+
+        private int readEntry(NameKey p) throws NoSuchProjectException {
+          return pluginCfg.getWithInheritance(p, "reviewers-by-blame")
+              .getInt(MAX_REVIEWERS, 3);
+        }
+      };
+    }
+  }
+
+  private Config readConfigFileFor(SitePaths site) {
+    File file = new File(site.etc_dir, "reviewers-by-blame.config");
+    FileBasedConfig cfg = new FileBasedConfig(file, FS.DETECTED);
+    if (!cfg.getFile().exists()) {
+      log.info(String.format("can not find config file: %s",
+          file.getAbsolutePath()));
+      return null;
+    }
+
+    if (cfg.getFile().length() == 0) {
+      log.info(String.format("empty config file: %s",
+          file.getAbsolutePath()));
+      return null;
+    }
+
+    try {
+      cfg.load();
+      return cfg;
+    } catch (ConfigInvalidException e) {
+      log.info(String.format(
+          "config file %s is invalid: %s",
+          cfg.getFile(), e.getMessage()), e);
+    } catch (IOException e) {
+      log.info(String.format("cannot read %s: %s",
+          cfg.getFile(), e.getMessage()), e);
+    }
+
+    return null;
   }
 
   @Override
   public void onGitReferenceUpdated(final Event e) {
-    Project.NameKey projectName = new Project.NameKey(e.getProjectName());
-
-    int maxReviewers;
-    try {
-      maxReviewers =
-          cfg.getWithInheritance(projectName, "reviewers-by-blame")
-             .getInt("maxReviewers", 3);
-    } catch (NoSuchProjectException x) {
-      log.error(x.getMessage(), x);
-      return;
-    }
-    if (maxReviewers <= 0) {
+    Project.NameKey project = new Project.NameKey(e.getProjectName());
+    if (!config.isEnabledFor(project)) {
       return;
     }
 
     Repository git;
     try {
-      git = repoManager.openRepository(projectName);
+      git = repoManager.openRepository(project);
     } catch (RepositoryNotFoundException x) {
       log.error(x.getMessage(), x);
       return;
@@ -126,7 +215,8 @@ class RefUpdateListener implements GitReferenceUpdatedListener {
               rw.parseCommit(ObjectId.fromString(u.getNewObjectId()));
 
           final Runnable task =
-              reviewersByBlameFactory.create(commit, change, ps, maxReviewers, git);
+              reviewersByBlameFactory.create(commit, change, ps,
+                  config.getMaxReviewersFor(project), git);
 
           workQueue.getDefaultQueue().submit(new Runnable() {
             public void run() {
--- src/main/resources/Documentation/config.md
+++ src/main/resources/Documentation/config.md
@@ -1,7 +1,54 @@
 Configuration
 =============
 
-The configuration of the @PLUGIN@ plugin is done on project level in
+Two level of configurations are supported by this plugin. The optional file
+`$site_path/etc/reviewers-by-blame.config` controls the function of this
+plugin.
+
+gerrit.explicitlyEnableProjects
+:       If true, project must be explictly enabled to add reviewers to
+        changes for this project.  By default, true.
+
+project.NAME.enabled
+:       If true, this project is enabled.  By default, false.
+
+project.NAME.maxReviewers
+:       The maximum number of reviewers that should be added to a change by
+	this plugin.  By default 3.
+
+Example 1: For all project on the site enable default behaivour: all projects
+enabled and `maxReviewers` = 3:
+
+```
+  [gerrit]
+    explicitlyEnableProjects = false
+```
+Example 2: Enable only project `foo`, all other projects are disabled:
+
+```
+  [project "foo"]
+    enabled = true
+    maxReviewers = 5
+```
+
+Example 3: Enable project `foo`, but disable project `bar`:
+
+```
+  [project "foo"]
+    enabled = true
+    maxReviewers = 5
+
+  [project "bar"]
+    enabled = false
+    maxReviewers = 7
+
+```
+
+If the configuration file as descried above doesn't exist, then the
+configuration can be provided in `project.config` file per project base.
+
+If the configuration file as descried above doesn't exist, then the
+configuration of the @PLUGIN@ plugin is done on project level in
 the `project.config` file of the project. Missing values are inherited
 from the parent projects. This means a global default configuration can
 be done in the `project.config` file of the `All-Projects` root project.
