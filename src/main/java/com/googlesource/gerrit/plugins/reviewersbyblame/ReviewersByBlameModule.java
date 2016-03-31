f3f5486e4a8b214b3396231ae88c900c1c711662
Sebastian Klapp <sebastian.klapp@gmail.com>
2016-03-31 17:29:45 +0200

    Add options ignoreDrafts and ignoreShortMessageRegEx
    Adding options to ignore certain commits when adding reviewers.
    This is quite useful if the committer knows that the
    commit is not in a reviewable state but already wants to push it.
    
    Change-Id: I7cffd46ce9d4b77b1710834bdf4f627879881073

--- src/main/java/com/googlesource/gerrit/plugins/reviewersbyblame/ReviewersByBlameModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/reviewersbyblame/ReviewersByBlameModule.java
@@ -31,5 +31,14 @@ public class ReviewersByBlameModule extends FactoryModule {
         .toInstance(new ProjectConfigEntry("Max Reviewers", 3, true,
             "The maximum number of reviewers that should be automatically added"
             + " to a change based on the git blame computation on the changed files."));
+    bind(ProjectConfigEntry.class).annotatedWith(Exports.named("ignoreDrafts"))
+        .toInstance(
+            new ProjectConfigEntry("Ignore Drafts", false,
+                "Ignore draft commits when adding reviewers"));
+    bind(ProjectConfigEntry.class)
+        .annotatedWith(Exports.named("ignoreShortMessageRegEx"))
+        .toInstance(
+            new ProjectConfigEntry("Ignore Regex", "", true,
+                "Ignore commits where the headline matches the given regular expression"));
   }
 }
