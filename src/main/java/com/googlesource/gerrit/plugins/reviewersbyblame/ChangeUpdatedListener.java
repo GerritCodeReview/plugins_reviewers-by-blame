faa66ebf81dadc964a6798f8d0ca80e4ecfc9f0f
Paladox none <thomasmulhall410@yahoo.com>
2019-01-18 00:19:55 +0000

    Support blacklisting users
    * Use case is where someone edit's a file but then dosen't edit it for
       years, they would not want to be added as a reviewer.
    
    We need this at the WMF where this feature has been requested.
    
    Change-Id: Iebedada9abea8a9a3e21c21faae386afa884b2b0

--- src/main/java/com/googlesource/gerrit/plugins/reviewersbyblame/ChangeUpdatedListener.java
+++ src/main/java/com/googlesource/gerrit/plugins/reviewersbyblame/ChangeUpdatedListener.java
@@ -14,7 +14,7 @@
 
 package com.googlesource.gerrit.plugins.reviewersbyblame;
 
-import com.google.gerrit.common.EventListener;
+import com.google.common.collect.ImmutableList;
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.reviewdb.client.PatchSet;
@@ -22,13 +22,14 @@ import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.reviewdb.server.ReviewDb;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.config.PluginConfigFactory;
 import com.google.gerrit.server.events.Event;
+import com.google.gerrit.server.events.EventListener;
 import com.google.gerrit.server.events.PatchSetCreatedEvent;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.WorkQueue;
 import com.google.gerrit.server.project.NoSuchProjectException;
-import com.google.gerrit.server.query.change.ChangeData;
 import com.google.gerrit.server.util.RequestContext;
 import com.google.gerrit.server.util.ThreadLocalRequestContext;
 import com.google.gwtorm.server.OrmException;
@@ -37,6 +38,7 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.ProvisionException;
 import java.io.IOException;
+import java.util.Arrays;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -56,7 +58,6 @@ class ChangeUpdatedListener implements EventListener {
   private final SchemaFactory<ReviewDb> schemaFactory;
   private final PluginConfigFactory cfg;
   private final String pluginName;
-  private final ChangeData.Factory changeDataFactory;
   private ReviewDb db;
 
   @Inject
@@ -68,7 +69,6 @@ class ChangeUpdatedListener implements EventListener {
       final ThreadLocalRequestContext tl,
       final SchemaFactory<ReviewDb> schemaFactory,
       final PluginConfigFactory cfg,
-      final ChangeData.Factory changeDataFactory,
       final @PluginName String pluginName) {
     this.reviewersByBlameFactory = reviewersByBlameFactory;
     this.repoManager = repoManager;
@@ -77,7 +77,6 @@ class ChangeUpdatedListener implements EventListener {
     this.tl = tl;
     this.schemaFactory = schemaFactory;
     this.cfg = cfg;
-    this.changeDataFactory = changeDataFactory;
     this.pluginName = pluginName;
   }
 
@@ -92,6 +91,7 @@ class ChangeUpdatedListener implements EventListener {
     int maxReviewers;
     String ignoreSubjectRegEx;
     String ignoreFileRegEx;
+    String[] ignoredUsers;
     try {
       maxReviewers =
           cfg.getFromProjectConfigWithInheritance(projectName, pluginName)
@@ -102,6 +102,9 @@ class ChangeUpdatedListener implements EventListener {
       ignoreFileRegEx =
           cfg.getFromProjectConfigWithInheritance(projectName, pluginName)
               .getString("ignoreFileRegEx", "");
+      ignoredUsers =
+          cfg.getFromProjectConfigWithInheritance(projectName, pluginName)
+              .getStringList("ignoredUsers", null);
     } catch (NoSuchProjectException x) {
       log.error(x.getMessage(), x);
       return;
@@ -113,20 +116,18 @@ class ChangeUpdatedListener implements EventListener {
     try (Repository git = repoManager.openRepository(projectName);
         RevWalk rw = new RevWalk(git);
         ReviewDb reviewDb = schemaFactory.open()) {
-      Change.Id changeId = new Change.Id(e.change.get().number);
-      final ChangeData cd = changeDataFactory.create(reviewDb, projectName, changeId);
-      if (cd == null) {
-        log.warn(
-            "Change with id: '{}' on project key: '{}' not found.",
-            changeId.get(),
-            projectName.toString());
+      Change.Id changeId = new Change.Id(Integer.parseInt(Integer.toString(e.change.get().number)));
+      PatchSet.Id psId =
+          new PatchSet.Id(changeId, Integer.parseInt(Integer.toString(e.patchSet.get().number)));
+      PatchSet ps = reviewDb.patchSets().get(psId);
+      if (ps == null) {
+        log.warn("Patch set " + psId.get() + " not found.");
         return;
       }
-      final Change change = cd.change();
-      PatchSet.Id psId = new PatchSet.Id(changeId, e.patchSet.get().number);
-      PatchSet ps = cd.patchSet(psId);
-      if (ps == null) {
-        log.warn("Patch set {} not found in change {}.", psId.get(), changeId.get());
+
+      final Change change = reviewDb.changes().get(psId.getParentKey());
+      if (change == null) {
+        log.warn("Change " + changeId.get() + " not found.");
         return;
       }
 
@@ -136,8 +137,9 @@ class ChangeUpdatedListener implements EventListener {
         return;
       }
 
+      ignoredUsersArray = ignoreUsers == null ? ImmutableList.of() : Arrays.asList(ignoreUsers);
       final Runnable task =
-          reviewersByBlameFactory.create(commit, change, ps, maxReviewers, git, ignoreFileRegEx);
+          reviewersByBlameFactory.create(commit, change, ps, maxReviewers, git, ignoreFileRegEx, ignoredUsersArray);
 
       workQueue
           .getDefaultQueue()
