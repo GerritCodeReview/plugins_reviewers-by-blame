6f73bb8314d4148b93ca988da1ecc985a27f7a47
Paladox none <thomasmulhall410@yahoo.com>
2019-01-20 14:51:50 +0000

    Add support for "Suggest Reviewer" action
    * This introduces a new config to disable automatic adding of
       reviewers, rather use a action to add them.
    
    Bug: Issue 10337
    Change-Id: I6e66b9c04c552e9928e10ddbd2ae72e1f12b1465

--- src/main/java/com/googlesource/gerrit/plugins/reviewersbyblame/ReviewersByBlameModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/reviewersbyblame/ReviewersByBlameModule.java
@@ -14,17 +14,24 @@
 
 package com.googlesource.gerrit.plugins.reviewersbyblame;
 
+import static com.google.gerrit.server.change.RevisionResource.REVISION_KIND;
+
 import com.google.gerrit.common.EventListener;
 import com.google.gerrit.extensions.annotations.Exports;
 import com.google.gerrit.extensions.api.projects.ProjectConfigEntryType;
 import com.google.gerrit.extensions.config.FactoryModule;
 import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.extensions.restapi.RestApiModule;
+import com.google.gerrit.extensions.webui.JavaScriptPlugin;
+import com.google.gerrit.extensions.webui.WebUiPlugin;
 import com.google.gerrit.server.config.ProjectConfigEntry;
 
 public class ReviewersByBlameModule extends FactoryModule {
   @Override
   protected void configure() {
-    DynamicSet.bind(binder(), EventListener.class).to(ChangeUpdatedListener.class);
+    if (!config.getBoolean("reviewers-by-blame", null, "enableSuggestReviewers", false) {
+      DynamicSet.bind(binder(), EventListener.class).to(ChangeUpdatedListener.class);
+    }
     factory(ReviewersByBlame.Factory.class);
     bind(ProjectConfigEntry.class)
         .annotatedWith(Exports.named("maxReviewers"))
@@ -61,5 +68,15 @@ public class ReviewersByBlameModule extends FactoryModule {
                 null,
                 false,
                 "Ignores users that  match list specified."));
+    if (config.getBoolean("reviewers-by-blame", null, "enableSuggestReviewers", false) {
+      install(
+          new RestApiModule() {
+            @Override
+            protected void configure() {
+              get(REVISION_KIND, "reviewers-by-blame").to(ReviewersByBlameAction.class);
+            }
+          });
+      DynamicSet.bind(binder(), WebUiPlugin.class).toInstance(new JavaScriptPlugin("reviewers-by-blame.js"));
+    }
   }
 }
